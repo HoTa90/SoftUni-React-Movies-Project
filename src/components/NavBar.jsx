4bc9dd92605ab5d4b9679e72fc1c9f497a6ede74
Aleksander Dimitrov <alexander.dimitrov90@gmail.com>
2025-03-16 18:18:01 +0200

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -34,8 +34,9 @@ export default function NavBar() {
         <div className="dropdown dropdown-end">
           <div tabIndex={0} role="button" className="btn btn-ghost m-1">
 
-            <UserIcon className="w-5 h-5 text-gray-300" />
-            Profile</div>
+            <UserIcon className="w-6 h-6 text-gray-300" />
+            <span className="text-xl">Profile</span>
+            </div>
           <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
             {!user ?
               <>
