de3646b30da626b9ba5f97876516e6588178ff32
Aleksander Dimitrov <alexander.dimitrov90@gmail.com>
2025-03-16 16:43:55 +0200

    create AuthContext

---
+++ src/context/auth/AuthContext.js
@@ -0,0 +1,42 @@
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "../../services/firebase.js";
+
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser)
+        });
+
+        return () => {
+            unsubscribe()
+        }
+    }, [])
+
+    const register = async (email, password) => {
+        const userData = await createUserWithEmailAndPassword(auth, email, password);
+        return userData
+    }
+
+    const login = async (email, password) => {
+        const userData = await signInWithEmailAndPassword(auth, email, password); 
+        return userData
+    }
+
+    const logout = async () => {
+        return await signOut(auth)
+    }
+
+    return (
+        <AuthContext.Provider value={{user, register, login, logout}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export const useAuth = () => useContext(AuthContext);
