d6de56d6b2e4d91be230f071ba541b3c592cbd2a
Aleksander Dimitrov <alexander.dimitrov90@gmail.com>
2025-03-16 12:31:14 +0200

    Update PopularSection.jsx

--- src/components/home/PopularSection.jsx
+++ src/components/home/PopularSection.jsx
@@ -14,8 +14,8 @@ export default function PopularSection({ movieList, errorMessage, isLoading }) {
                 const [seriesData, peopleData] = await Promise.all(
                     [getTrending('tv'), getTrending('person')]
                 );
-                setSeries(seriesData);  
-                setPeople(peopleData);  
+                setSeries(seriesData);
+                setPeople(peopleData);
             } catch (err) {
                 console.error("Error fetching data:", err);
             }
@@ -23,7 +23,7 @@ export default function PopularSection({ movieList, errorMessage, isLoading }) {
 
         fetchData();
     }, []);
-    
+
     return (
         <section className="px-28 py-15">
             {/* Popular Movies Section */}
@@ -33,34 +33,37 @@ export default function PopularSection({ movieList, errorMessage, isLoading }) {
             ) : errorMessage ? (
                 <p className="text-red-500">{errorMessage}</p>
             ) : (
-                <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 mb-12">
-                    {movieList && movieList.slice(0, 5).map(movie => (
-                        <li key={movie.id} className="p-0"> {/* Remove padding here */}
-                            <HeroCard data={movie} type='movie' />
-                        </li>
-                    ))}
-                </ul>
+                <>
+                    <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 mb-12">
+                        {movieList && movieList.slice(0, 5).map(movie => (
+                            <li key={movie.id} className="p-0">
+                                <HeroCard data={movie} type='movie' />
+                            </li>
+                        ))}
+                    </ul>
+                    {/* TV Section */}
+                    <h2 className="text-white text-2xl font-bold mb-8">TV</h2>
+                    <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 mb-12">
+                        {series && series.slice(0, 5).map(tv => (
+                            <li key={tv.id} className="p-0">
+                                <HeroCard data={tv} type='tv' />
+                            </li>
+                        ))}
+                    </ul>
+
+                    {/* People Section */}
+                    <h2 className="text-white text-2xl font-bold mb-8">People</h2>
+                    <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
+                        {people && people?.slice(0, 5).map(p => (
+                            <li key={p.id} className="p-0">
+                                <HeroCard data={p} type='person' />
+                            </li>
+                        ))}
+                    </ul>
+                </>
             )}
 
-            {/* TV Section */}
-            <h2 className="text-white text-2xl font-bold mb-8">TV</h2>
-            <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 mb-12">
-                {series && series.slice(0, 5).map(tv => (
-                    <li key={tv.id} className="p-0"> {/* Remove padding here */}
-                        <HeroCard data={tv} type='tv' />
-                    </li>
-                ))}
-            </ul>
 
-            {/* People Section */}
-            <h2 className="text-white text-2xl font-bold mb-8">People</h2>
-            <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
-                {people && people?.slice(0, 5).map(p => (
-                    <li key={p.id} className="p-0"> {/* Remove padding here */}
-                        <HeroCard data={p} type='person' />
-                    </li>
-                ))}
-            </ul>
         </section>
     );
 }
