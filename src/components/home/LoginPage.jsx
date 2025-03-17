faa76f3dceb3cb5468e729cd5c55dada08b15225
Aleksander Dimitrov <alexander.dimitrov90@gmail.com>
2025-03-17 14:28:09 +0200

    fixed loading and error handling in login

--- src/components/home/LoginPage.jsx
+++ src/components/home/LoginPage.jsx
@@ -5,24 +5,37 @@ import { useAuth } from "../../context/AuthContext.jsx";
 
 
 export default function LoginPage() {
-    const { login, isLoading } = useAuth()
+    const { login } = useAuth()
     const navigate = useNavigate()
     const [error, setError] = useState(null)
+    const [isLoading, setIsLoading] = useState(false)
 
 
-    const loginHandler = async (formdata) => {
+    const loginHandler = async (event) => {
 
-        setError(null)
-        const { email, password } = Object.fromEntries(formdata)
+        event.preventDefault();
+        setError(null);
+        setIsLoading(true);
+
+        const formData = new FormData(event.target);
+        const { email, password } = Object.fromEntries(formData);
         try {
-            await login(email, password)
-            navigate('/')
-        } catch (err) {
-            setError(err.message)
-            console.log(err.message)
-        }
-    }
+            await login(email, password);
 
+            navigate("/");
+        } catch (err) {
+                console.log(err.code)
+            if (err.code === 'auth/invalid-credential') {
+                setError('Invalid email or password.');
+            } else {
+                setError(err.message);
+            }
+        } finally {
+            event.target.reset()
+            setIsLoading(false);
+
+        }
+    };
     return (
         <div
             className="min-h-screen flex items-center justify-center relative"
@@ -33,10 +46,9 @@ export default function LoginPage() {
             }}
         >
             <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/50"></div>
-            {isLoading && <Spinner />}
 
 
-            <form action={loginHandler} className="w-full max-w-md p-6 bg-[#2c2c2c] rounded-box shadow-lg relative z-10">
+            <form onSubmit={loginHandler} className="w-full max-w-md p-6 bg-[#2c2c2c] rounded-box shadow-lg relative z-10">
                 <fieldset className="border border-base-300 p-4 rounded-box">
                     <legend className="text-xl font-bold text-center mb-4">Login</legend>
 
@@ -71,10 +83,11 @@ export default function LoginPage() {
                                 </Link>
                             </p>
                             {error && <p className="text-red-500">{error}</p>}
+                            {isLoading && <Spinner />}
                         </div>
 
-                        <button type="submit" className="btn w-full mt-6">
-                            Login
+                        <button type="submit" className="btn w-full mt-6" disabled={isLoading}>
+                            {isLoading ? "Loggin in..." : "Login"}
                         </button>
                     </div>
                 </fieldset>
